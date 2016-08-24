a7dd4fa3dffde0d8d8d13ea66f3f9333f3ec6959
Isaac Westwood <roobubba@gmail.com>
2016-08-24 16:58:50 +0000

    do not require link

--- app/models/microbe.rb
+++ app/models/microbe.rb
@@ -1,7 +1,7 @@
 class Microbe < ActiveRecord::Base
 
   validates :name, presence: true, length: {minimum: 3, maximum: 40 }
-  validates :link, presence: true, length: {minimum: 10, maximum: 150 }
+  #validates :link, presence: true, length: {minimum: 10, maximum: 150 }
   validates :cost, presence: true
   validates :fullname, presence: true, length: {minimum: 3, maximum: 60 }
   

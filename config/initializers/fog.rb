4d50650cde70ead3780cf76228d69fc9d3a82d57
Isaac Westwood <roobubba@gmail.com>
2016-09-14 14:06:14 +0000

    testing carrierwave-aws

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,14 +1,16 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.storage = :fog
-    config.fog_credentials = {
-    :provider => 'AWS',
-    :aws_access_key_id => ENV['S3_ACCESS_KEY'],
-    :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+    config.storage = :aws
+    config.aws_credentials = {
+    #:provider => 'AWS',
+    :access_key_id => ENV['S3_ACCESS_KEY'],
+    :secret_access_key => ENV['S3_SECRET_KEY'],
     :region => ENV['S3_REGION']
     }
     
-    config.fog_directory = ENV['S3_BUCKET']
+    config.aws_bucket = ENV['S3_BUCKET']
+    config.aws_acl = :public_read
+    #config.fog_directory = ENV['S3_BUCKET']
     #config.fog_public = false
   end
 end
